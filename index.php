5f0e847a1bdbdc93bb630bedd0a527d3e31e72ff
brunogabriel <brunogabriel58@outlook.com>
2021-06-03 19:08:53 -0300

    Adicionado Novos Styles no Index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>SubSubMarinho</title>
+	<title>Jaguar Motors</title>
 	<meta charset="utf-8">
 
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
@@ -23,69 +23,79 @@
   <script type="text/javascript" src="js/parsley.min.js"></script>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="index.php">
-  	<img src="images/logo.png" alt="SubMarino">
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container text-center">
+    <a class="navbar-brand" href="index.php">
+      <img src="images/logo3.png" alt="SubMarino">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-  <div class="collapse navbar-collapse" id="menu">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="index.php">
-        	Home
-    	</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="index.php?pagina=sobre">
-        	Sobre a Empresa
-    	</a>
-      </li>
-      <?php
-      	//sql para selecionar as categorias
-      	$sql = "select * from categoria order by categoria";
-      	//executar este sql
-      	$result = mysqli_query($con, $sql);
-      	//recuperar os dados por linha
-      	while ( $dados = mysqli_fetch_array( $result ) ){
-
-      		//separar os resultados
-      		$id = $dados["id"];
-      		$categoria = $dados["categoria"];
-      		//echo "<p>{$id} {$categoria}</p>";
-      		echo "<li class=\"nav-item\">
-		        <a class=\"nav-link\" href=\"index.php?pagina=categoria&id={$id}\">
-		        	{$categoria}
-		    	</a>
-		      </li>";
-
-      	}
-      ?>
-    </ul>
-
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a href="index.php?pagina=login" class="nav-link">
-          <i class="fas fa-user"></i>
+    <div class="collapse navbar-collapse" id="menu">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="veiculos.php">
+          <strong>Veículos</strong>
         </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="index.php?pagina=carrinho">
-          <i class="fas fa-shopping-cart"></i>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="vendasfinanc.php">
+          <strong>Vendas e Financiamento</strong>
         </a>
-      </li>
-    </ul>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="ofertas.php">
+          <strong>Ofertas</strong>
+        </a>
+        </li>
+        
+          <!--//sql para selecionar as categorias
+          $sql = "select * from categoria order by categoria";
+          //executar este sql
+          $result = mysqli_query($con, $sql);
+          //recuperar os dados por linha
+          while ( $dados = mysqli_fetch_array( $result ) ){
+
+            //separar os resultados
+            $id = $dados["id"];
+            $categoria = $dados["categoria"];
+            //echo "<p>{$id} {$categoria}</p>";
+            echo "<li class=\"nav-item\">
+              <a class=\"nav-link\" href=\"index.php?pagina=categoria&id={$id}\">
+                {$categoria}
+            </a>
+            </li>";
+
+          }-->
+        
+
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?pagina=sobre">
+            <strong>Sobre a Jaguar</strong>
+        </a>
+        </li>
+      </ul>
+
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a href="index.php?pagina=login" class="nav-link">
+            <i class="fas fa-user"></i>
+          </a>
+        </li>
+      </ul>
 
 
-    <form class="form-inline my-2 my-lg-0" name="formBusca" action="index.php">
-      <input class="form-control mr-sm-2" type="search" placeholder="Palavra-chave" aria-label="Search" name="busca">
-      <button class="btn btn-warning my-2 my-sm-0" type="submit">
-      	<i class="fas fa-search"></i>
-      </button>
-    </form>
-  </div>
+      <form class="form-inline my-2 my-lg-0" name="formBusca">
+        <input class="form-control mr-sm-2" type="search" placeholder="Palavra-chave" aria-label="Search" name="busca">
+        <button class="btn btn-dark my-2 my-sm-0 dropdown">
+          <i class="fas fa-search"></i>
+        </button>
+      </form>
+    </div>
+  </div>  
 </nav>
 
 <main class="container">
@@ -108,14 +118,11 @@
 	?>
 </main>
 
-<footer class="bg-primary">
+<footer class="bg-dark">
   <div class="container">
-    <p class="text-center">SubSubmarino - Comércio eletrônico</p>
+    <p class="text-center">Jaguar Motors - Concessionária</p>
     <p class="text-center">
-      <strong>Meios de Pagamento:</strong>
-    </p>
-    <p class="text-center">
-      <img src="images/pagamentos.png" alt="Meios de pagamento">
+      <strong>Retire seu Carro na Hora! - Fone (44) 3663-9823</strong>
     </p>
     <hr>
   </div>
